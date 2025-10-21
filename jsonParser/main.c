7b63b6a968846797a75f347873ea8d1f70387ff5
MortenStaunstrup <mort490fgromling@gmail.com>
2025-10-21 20:38:10 +0200

    More work on the parser

--- jsonParser/main.c
+++ jsonParser/main.c
@@ -174,7 +174,6 @@ int main(int argc, char *argv[]) {
                     printf("UNKNOWN TOKEN\n");
             }
             tok++; // Move to the next token in the array
-
         }
 
         if (result.result == SUCCESS) {
