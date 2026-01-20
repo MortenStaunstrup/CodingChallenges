95facf3a382d64624dbbe9ce0a5e75ffebcf9acb
MortenStaunstrup <mort490fgromling@gmail.com>
2026-01-20 14:41:49 +0100

    Init commit of load balancer. C Windows server

---
+++ LoadBalancer/Server/main.c
@@ -0,0 +1,82 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#define PORT 80
+#define BUFFER_SIZE 1024
+
+int main(void) {
+    WSADATA wsa;
+    SOCKET server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
+        printf("WSAStartup failed: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == INVALID_SOCKET) {
+        printf("Could not create socket: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    // Set socket option
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt)) < 0) {
+        printf("setsockopt failed: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.S_un.S_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == SOCKET_ERROR) {
+        printf("bind failed: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(server_fd, 3) == SOCKET_ERROR) {
+        printf("listen failed: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen);
+    if (new_socket == INVALID_SOCKET) {
+        printf("accept failed: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Connection accepted\n");
+
+    int valread;
+    while((valread = recv(new_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+        printf("Client: %s", buffer);
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    closesocket(new_socket);
+    closesocket(server_fd);
+    WSACleanup();
+
+    return 0;
+
+}
